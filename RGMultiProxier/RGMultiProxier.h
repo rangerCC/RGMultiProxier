5d7d8c982a14fd946d83e3046ab681c465c2cfe2
TingtingYan <sunny@sunny.lan>
2018-02-23 01:14:19 +0800

    push proj

---
+++ RGMultiProxier/RGMultiProxier.h
@@ -0,0 +1,19 @@
+//
+//  RGMultiProxier.h
+//  RGMultiProxier
+//
+//  Created by TingtingYan on 2018/2/23.
+//  Copyright © 2018年 ranger. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for RGMultiProxier.
+FOUNDATION_EXPORT double RGMultiProxierVersionNumber;
+
+//! Project version string for RGMultiProxier.
+FOUNDATION_EXPORT const unsigned char RGMultiProxierVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <RGMultiProxier/PublicHeader.h>
+
+
